988e9d42dbdd41b34efba2a9bf20090a4c3bcde1
Tomoki WATANABE <tomoki_watanabe@r.recruit.co.jp>
2021-08-03 19:40:54 +0900

    Chapter 6.12

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -179,7 +179,7 @@ public class EmployeeAction extends ActionBase {
 
     /**
      * 更新を行う
-     * 
+     *
      * @throws ServletException
      * @throws IOException
      */
@@ -219,6 +219,28 @@ public class EmployeeAction extends ActionBase {
                 // 一覧画面にリダイレクト
                 redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
             }
+        }
+    }
+
+    /**
+     * 論理削除を行う
+     * 
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void destroy() throws ServletException, IOException {
+
+        // CSRF対策 tokenのチェック
+        if (checkToken()) {
+
+            // idを条件に従業員データを論理削除する
+            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+            // セッションに削除完了のフラッシュメッセージを設定
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+
+            // 一覧画面にリダイレクト
+            redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
         }
     }
 
